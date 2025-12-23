cb50dc41480591e1634c3b11017d06355f42ba21
Simran Verma <vermasimran327@gmail.com>
2025-12-23 22:00:48 +0530

    home updation

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <img  className = 'w-16 absolute left-5 top-5'src="https://tse4.mm.bing.net/th/id/OIP.nVsbF4Ci20QIuQYTkO9XLAHaD2?pid=Api&h=220&P=0" alt="" />
+
+
+      <div className='h-screen w-screen'>
+        <img src="" alt="" />
+      </div>
+    </div>
   )
 }
 
