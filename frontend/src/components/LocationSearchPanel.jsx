ccac635ded06171349a812c892f0d5a276167432
Simran Verma <vermasimran327@gmail.com>
2025-12-25 21:33:51 +0530

    locationSearchPanel

--- frontend/src/components/LocationSearchPanel.jsx
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -5,28 +5,28 @@ const LocationSearchPanel = () => {
     <div>
         {/* sample data*/}
 
-        <div className = 'flex  gap-2 my-4 items-center justify-start'>
+        <div className = 'flex  gap-2 border-2 p-3 border-white active:border-black rounded-xl  my-4 items-center justify-start'>
             <h2 className='bg-[#eee] font-medium h-10 items-center flex justify-center w-10 rounded-full'><i className="ri-map-pin-line"></i></h2>
             <h4 className ='font-medium'>
                 Circular road ,Near Canara Bank, Nagla Emily, Hathras,U.P
             </h4>
         </div>
 
-         <div className = 'flex  gap-2 my-4 items-center justify-start'>
+         <div className = 'flex  gap-2 border-2 p-3 border-white active:border-black rounded-xl my-4 items-center justify-start'>
             <h2 className='bg-[#eee] font-medium h-10 items-center flex justify-center w-10 rounded-full'><i className="ri-map-pin-line"></i></h2>
             <h4 className ='font-medium'>
                 Circular road ,Near Canara Bank, Nagla Emily, Hathras,U.P
             </h4>
         </div>
 
-         <div className = 'flex  gap-2 my-4 items-center justify-start'>
+         <div className = 'flex  gap-2 border-2 p-3 border-white active:border-black rounded-xl my-4 items-center justify-start'>
             <h2 className='bg-[#eee] font-medium h-10 items-center flex justify-center w-10 rounded-full'><i className="ri-map-pin-line"></i></h2>
             <h4 className ='font-medium'>
                 Circular road ,Near Canara Bank, Nagla Emily, Hathras,U.P
             </h4>
         </div>
 
-         <div className = 'flex  gap-2 my-4 items-center justify-start'>
+         <div className = 'flex  gap-2 border-2 p-3 border-white active:border-black rounded-xl my-4 items-center justify-start'>
             <h2 className='bg-[#eee] font-medium h-10 items-center flex justify-center w-10 rounded-full'><i className="ri-map-pin-line"></i></h2>
             <h4 className ='font-medium'>
                 Circular road ,Near Canara Bank, Nagla Emily, Hathras,U.P
