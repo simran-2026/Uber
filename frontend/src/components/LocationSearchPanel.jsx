9c56705e8e42487ae35db2677dbb5c7441c05a92
Simran Verma <vermasimran327@gmail.com>
2025-12-25 20:04:30 +0530

    latest

---
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const LocationSearchPanel = () => {
+  return (
+    <div>
+        {/* sample data*/}
+
+        <div className = 'flex  gap-2 my-4 items-center justify-start'>
+            <h2 className='bg-[#eee] font-medium h-10 items-center flex justify-center w-10 rounded-full'><i className="ri-map-pin-line"></i></h2>
+            <h4 className ='font-medium'>
+                Circular road ,Near Canara Bank, Nagla Emily, Hathras,U.P
+            </h4>
+        </div>
+
+         <div className = 'flex  gap-2 my-4 items-center justify-start'>
+            <h2 className='bg-[#eee] font-medium h-10 items-center flex justify-center w-10 rounded-full'><i className="ri-map-pin-line"></i></h2>
+            <h4 className ='font-medium'>
+                Circular road ,Near Canara Bank, Nagla Emily, Hathras,U.P
+            </h4>
+        </div>
+
+         <div className = 'flex  gap-2 my-4 items-center justify-start'>
+            <h2 className='bg-[#eee] font-medium h-10 items-center flex justify-center w-10 rounded-full'><i className="ri-map-pin-line"></i></h2>
+            <h4 className ='font-medium'>
+                Circular road ,Near Canara Bank, Nagla Emily, Hathras,U.P
+            </h4>
+        </div>
+
+         <div className = 'flex  gap-2 my-4 items-center justify-start'>
+            <h2 className='bg-[#eee] font-medium h-10 items-center flex justify-center w-10 rounded-full'><i className="ri-map-pin-line"></i></h2>
+            <h4 className ='font-medium'>
+                Circular road ,Near Canara Bank, Nagla Emily, Hathras,U.P
+            </h4>
+        </div>
+
+
+    </div>
+  )
+}
+
+export default LocationSearchPanel
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React,{useRef, useState} from 'react'
 import {useGSAP} from '@gsap/React'
 import gsap from 'gsap'
 import 'remixicon/fonts/remixicon.css'
+import LocationSearchPanel from '../components/LocationSearchPanel'
 
 
 const Home = () => {
@@ -18,12 +19,20 @@ const Home = () => {
     useGSAP(function(){
       if(panelOpen){
         gsap.to(panelRef.current,{
-        height:'70%' 
+        height:'70%', 
+       
+      })
+      gsap.to(panelCloseRef.current,{
+        opacity:1
       })
       }else{
         gsap.to(panelRef.current,{
-          height:'0%'
+          height:'0%',
+        
         })
+        gsap.to(panelCloseRef.current,{
+        opacity: 0
+      })
       }
     },[panelOpen])
 
@@ -31,7 +40,7 @@ const Home = () => {
 
 
   return (
-    <div className='h-screen relative'>
+    <div className='h-screen relative overflow-hidden'>
       <img  className = 'w-16 absolute left-5 top-5'src="https://tse4.mm.bing.net/th/id/OIP.nVsbF4Ci20QIuQYTkO9XLAHaD2?pid=Api&h=220&P=0" alt="" />
 
 
@@ -44,7 +53,7 @@ const Home = () => {
          
         <h5 ref={panelCloseRef} onClick={()=>{
           setpanelOpen(false)
-        }} className='absolute right-6 top-6 text-xl'>
+        }} className='absolute opacity-0 right-6 top-6 text-xl'>
           <i className="ri-arrow-down-wide-line"></i>
           </h5> 
          
@@ -52,7 +61,7 @@ const Home = () => {
         <form  onSubmit={(e) =>{
           submitHandler(e)
         }} >
-          <div className='line absolute h-11 w-1 top-[35%]  left-10 bg-gray-900 rounded-full'></div>
+          <div className=' line absolute h-11 w-1 top-[38%]  left-10 bg-gray-900 rounded-full'></div>
           <input 
           onClick={()=>{
             setpanelOpen(true)
@@ -61,7 +70,7 @@ const Home = () => {
           onChange={(e) =>{
             setPickup(e.target.value)
           }}
-          className='bg-[#eee] px-12 py-2 text-base rounded-lg w-full mt-5'  
+          className='bg-[#eee] px-12 py-2 text-lg rounded-lg w-full mt-5'  
           type="text" 
           placeholder = 'Add a pick-up location' />
           <input 
@@ -72,20 +81,31 @@ const Home = () => {
           onChange={(e)=>{
             setDestination(e.target.value)
           }}
-          className='bg-[#eee] px-12 py-2 text-base rounded-lg w-full mt-2' 
+          className='bg-[#eee] px-12 py-2 text-lg rounded-lg w-full mt-2' 
           type="text" 
           placeholder = 'Enter your destination' />
         </form>
        </div>
        
-       <div  ref={panelRef} className='h-[70%] bg-red-500 p-5 '>
-
+       <div  ref={panelRef} className='bg-white h-0'>
+        <LocationSearchPanel/>
        </div>
       
       </div>
      
-
-
+    
+     <div className='fixed z-10 bottom-0 bg-white '>
+      <div className=' flex bg-red-600 item-center justify-between'>
+         <img  className ='h-10'src="https://tse3.mm.bing.net/th/id/OIP.Y_gX5xiVCzVgpmDuQ1h6ogHaEK?pid=Api&h=220&P=0" alt="" />
+      <div className=' bg-green-500 w-1/2'>
+        <h4 className='font-medium'>RideGo <span> <i className="ri-user-fill"></i>4</span></h4>
+        <h5>2 mins away </h5>
+        <p>Affordable, compact rides</p>
+      </div>
+      <h2 className=' text-2xl font-semibold'>₹192</h2>
+      </div>
+     
+     </div>
 
 
     </div>
